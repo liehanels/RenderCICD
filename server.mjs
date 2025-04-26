8a99f277bea0485dd74b13e4e0235f082cff914b
Liehan Els <liehan13@gmail.com>
2025-04-26 21:49:48 +0200

    Added some porting stuff

--- server.mjs
+++ server.mjs
@@ -26,8 +26,8 @@ mongoose
 app.use('/api', wordRoutes);
 
 // Start the server
-const server = app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+const server = app.listen(process.env.PORT || PORT, () => {
+    console.log(`Server is running on port ${process.env.PORT || PORT}`);
 });
 
 export { app, server };
